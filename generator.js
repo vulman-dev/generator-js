e0663fb6d80a5ac87bc29f0c2bc85c41b18d3595
Евгений Вульман <vulman@DESKTOP-0SKP5G9.localdomain>
2022-08-20 00:24:00 +0300

    new commit

---
+++ generator.js
@@ -0,0 +1,24 @@
+import { mkdir, mkfile } from '@hexlet/immutable-fs-trees';
+
+const generate = () => {
+    const tree = mkdir('nodejs-package', [
+        mkfile('Makefile'),
+        mkfile('README.md'),
+        mkdir('dist', []),
+        mkdir('__tests__', [
+          mkfile('half.test.js', { type: 'text/javascript' }),
+        ]),
+        mkfile('babel.config.js', { type: 'text/javascript' }),
+        mkdir('node_modules', [
+          mkdir('@babel', [
+            mkdir('cli', [
+              mkfile('LICENSE'),
+            ]),
+          ]),
+        ], { owner: 'root', hidden: false }),
+], { hidden: true });
+
+    return tree;
+};
+
+console.log(generate());
